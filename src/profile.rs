5c1e9ec3410f91374a49ed616e62b6ebd1ee71b8
thrombe <yash.n.thombare620@gmail.com>
2023-10-29 15:41:30 +0530

    don't try to sync stuff duplicated in multiple modules

--- src/profile.rs
+++ src/profile.rs
@@ -1,11 +1,16 @@
-use std::{collections::{HashMap, HashSet}, fs, path::PathBuf};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+    path::PathBuf,
+};
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use serde::{Deserialize, Serialize};
 
 use crate::{
     config::{Ctx, ProfileDesc},
-    module::Module,
+    entry::{RelativePath, STUB},
+    module::{Module, PathResolutionError},
 };
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -58,29 +63,98 @@ impl Profile {
         Ok(s)
     }
 
+    /// creates new symlinks for any entry that does not have a symlink
     pub fn sync(&self, force: bool, ctx: &Ctx) -> Result<()> {
         for name in self
             .active_conf
-            .modules.iter().collect::<HashSet<_>>()
+            .modules
+            .iter()
+            .collect::<HashSet<_>>()
             .difference(&self.required_conf.modules.iter().collect::<HashSet<_>>())
         {
             let module = self.modules.get(name.as_str()).expect("checked earlier");
             module.unlink_all(force, ctx)?;
         }
 
-        // TODO: don't try to sync stuff that is duplicated in multiple modules
         // TODO: give error when trying to add something to a module but other module already has the thing (only if other has higher precedence)
-        let mut synced_home = HashSet::new();
-        let mut synced_non_home = HashSet::new();
-        for name in self.required_conf.modules.iter() {
-            let module = self.modules.get(name).expect("checked earlier");
-            module.sync(force, ctx)?;
+        let mut synced = HashSet::new();
+        for name in self.required_conf.modules.iter().rev() {
+            let module = self.modules.get(name).expect("checked in Profile::new");
 
-            for e in module.home_entries.iter() {
-                synced_home.insert(e);
-            }
-            for e in module.non_home_entries.iter() {
-                synced_non_home.insert(e);
+            for e in module
+                .home_entries
+                .iter()
+                .map(|p| module.entry_from_relative(&RelativePath::Home(p.to_path_buf()), ctx))
+                .chain(module.non_home_entries.iter().map(|p| {
+                    module.entry_from_relative(&RelativePath::NonHome(p.to_path_buf()), ctx)
+                }))
+            {
+                let src = e.src.clone();
+                // ignore if already synced by a module with higher precedence
+                if synced.contains(&src) {
+                    continue;
+                }
+                synced.insert(src);
+
+                let privilege = e.get_priv(ctx)?;
+                fs::create_dir_all(e.src.parent().unwrap())?;
+                drop(privilege);
+
+                if !e.src.exists() {
+                    println!(
+                        "creating symlink\n  src: {:?}\n  dst: {:?}",
+                        &e.src, &e.dest
+                    );
+                    e.symlink_to_src(ctx)?;
+                    continue;
+                }
+
+                if e.src.canonicalize()? == e.dest {
+                    continue;
+                }
+
+                println!(
+                    "creating symlink\n  src: {:?}\n  dst: {:?}",
+                    &e.src, &e.dest
+                );
+
+                if !force {
+                    return Err(anyhow!(
+                        "there is already a file/dir at: {:?}. use -f flag to force sync",
+                        &e.src
+                    ));
+                }
+
+                let dump_to = ctx.dump_dir.join(e.relative.clone().relative());
+
+                println!(
+                    "moving contents to dump\n  src: {:?}\n  dump: {:?}",
+                    &e.src, &dump_to
+                );
+
+                fs::create_dir_all(dump_to.parent().unwrap())?;
+
+                if e.src.is_file() || e.src.is_symlink() {
+                    let _ = fs::copy(&e.src, &dump_to)?;
+                    e.rm_src_file(ctx)?;
+                } else if e.src.is_dir() {
+                    fs_extra::dir::copy(
+                        &e.src,
+                        &dump_to,
+                        &fs_extra::dir::CopyOptions::new()
+                            .copy_inside(false)
+                            .content_only(true),
+                    )?;
+                    e.rm_src_dir_all(ctx)?;
+                } else {
+                    return Err(anyhow!(
+                        "cannot handle this type of file or whatever: {:?}",
+                        &e.src
+                    ));
+                }
+                println!();
+
+                e.symlink_to_src(ctx)?;
             }
         }
 
